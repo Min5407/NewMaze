d74b79dfe5de0fe772ca352a8bd25f818948945e
Min young Go <min@mins-mbp.wireless.griffith.edu.au>
2019-09-27 19:08:24 +1000

    navmesh bug fixed

---
+++ MazeGame/Assets/Maze/assets/Maze Assets/alvin/BonusBoxHealth.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusBoxHealth : MonoBehaviour
+{
+    private GameObject player;
+    public int Health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        print(player.name);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0.5f, 0.7f, 0.5f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+           player.SendMessage("ApplyDamage", Health);
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ MazeGame/Assets/Maze/assets/Maze Assets/alvin/BonusBoxSpeed.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusBoxSpeed : MonoBehaviour
+{
+    private GameObject player;
+    public int Speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        print(player.name);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0.5f, 0.7f, 0.5f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+           player.SendMessage("AdjustSpeed", Speed);
+            Destroy(this.gameObject);
+        }
+    }
+}
